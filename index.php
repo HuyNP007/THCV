d09c16eead5a39497ca102892ea1ff67bfdbfebc
Phước Huy <nphuy2001186@student.ctuet.edu.vn>
2022-12-15 19:05:21 +0700

    ver2.1

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
- 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+     <form action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>Tên đăng nhập</label>
+     	<input type="text" name="tendangnhap" placeholder="User Name"><br>
+
+     	<label>Mật khẩu</label>
+     	<input type="matkhau" name="matkhau" placeholder="Password"><br>
+
+     	<button type="submit">Đăng nhập</button>
+     </form>
+</body>
+</html>
--- model/connectdb.php
+++ model/connectdb.php
@@ -1,17 +1,12 @@
 <?php
-    function connectiondb(){
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
 
-        try {
-        $conn = new PDO("mysql:host=$servername;dbname=thcv", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-        } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-        }
-        return $conn;
-    }
-?>
+$sname= "localhost";
+$unmae= "root";
+$password = "";
+$db_name = "thcv";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+	echo "Connection failed!";
+}
