7bf04403818cb4e56ac9d2c4e0d343418a0a6007
tuankhanhnguyen <120168953+tuankhanhnguyen@users.noreply.github.com>
2022-12-16 05:34:00 +0700

    NTK

---
+++ JS/sogie.js
@@ -0,0 +1,34 @@
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "Sexuality (tính dục): Một khái niệm bao gồm giới tính sinh học (có cơ thể là nam hay nữ), bản dạng giới (cảm nhận mình là nam hay nữ), xu hướng tính dục (yêu người cùng giới hay khác giới) và thể hiện giới (thể hiện namtính hay nữ tính), vân vân. Tính dục khác với tình dục.";
+  document.getElementById("demo").style.fontSize = "25px"; 
+  document.getElementById("demo").style.color = "black";
+  document.getElementById("demo").style.backgroundColor = "#4CAF50";
+}
+
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "Sexual Orientation (xu hướng tính dục):  Một yếu tố trong tính dục, thể hiện ở sự hấp dẫn có tính bền vững về cảm xúc, tình dục hoặc tình cảm hướng tới người cùng giới, khác giới hay cả hai. Từ đó có các xu hướng như đồng tính, dị tính hoặc song tính... Xu hướng tính dục của một người không nhất thiết trùng với hành vi tình dục của người đó. (Từ khác: khuynh hướng tính dục, thiên hướng tính dục)";
+  document.getElementById("demo").style.fontSize = "25px"; 
+  document.getElementById("demo").style.color = "black";
+  document.getElementById("demo").style.backgroundColor = "#008CBA";
+}
+
+function myFunction3() {
+  document.getElementById("demo").innerHTML = "Gender: giới tính xã hội được xác định theo cấu trúc xã hội thông thường, các hành vi, vai trò khác nhau trong xã hội theo thời gian. Gender thường được phân thành: nam, nữ hoặc không thuộc hai nhóm trên-nonbinary.";
+  document.getElementById("demo").style.fontSize = "25px"; 
+  document.getElementById("demo").style.color = "white";
+  document.getElementById("demo").style.backgroundColor = "#f44336";
+}
+
+function myFunction4() {
+  document.getElementById("demo").innerHTML = "Gender Identity (bản dạng giới): Một yếu tố trong tính dục, thể hiện ở cảm nhận về giới tính của một người. Bản dạng giới không nhất thiết phải trùng với giới tính sinh học. Bản dạng giới cũng độc lập với xu hướng tính dục, vì bản dạng giới liên quan tới việc một người nghĩ mình thuộc giới tính nào, còn xu hướng tính dục liên quan tới việc một người cảm thấy hấp dẫn với ai. (Từ khác: nhân dạng giới)";
+  document.getElementById("demo").style.fontSize = "25px"; 
+  document.getElementById("demo").style.color = "black";
+  document.getElementById("demo").style.backgroundColor = "#e7e7e7";
+}
+
+function myFunction5() {
+  document.getElementById("demo").innerHTML = "Gender Expression (Thể hiện giới): Là cách mà một người thể hiện giới tính xã hội của mình thông qua hành vi cư xử, cách ăn mặc, giọng nói và những đặc điểm khác. Xã hội nhìn nhận những dấu hiệu này để phân loại giữa giới tính nam hoặc nữ, mặc dù cách đánh giá nam hay nữ cũng được thay đổi theo thời gian và theo văn hoá địa phương.";
+  document.getElementById("demo").style.fontSize = "25px"; 
+  document.getElementById("demo").style.color = "white";
+  document.getElementById("demo").style.backgroundColor = "#555555";
+}
